984567e64d7b320f4522b74737a972212669b501
nigel0913 <nigel0913@gmail.com>
2014-03-29 14:45:10 +0800

    add LockService

---
+++ src/com/nigel/service/LockService.java
@@ -0,0 +1,15 @@
+package com.nigel.service;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+public class LockService extends Service {
+
+	@Override
+	public IBinder onBind(Intent arg0) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
