994829ce5aeb144e61ea28829a2cfda54981bea5
nigel0913 <nigel0913@gmail.com>
2014-04-16 09:51:46 +0800

    can save threshold
    but not used in recognization

--- src/com/nigel/ecustlock/ConfigActivity.java
+++ src/com/nigel/ecustlock/ConfigActivity.java
@@ -4,12 +4,19 @@ import android.app.Activity;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.View.OnFocusChangeListener;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class ConfigActivity extends Activity {
 	
 	EditText etThreshold;
+	Button btnSave;
 	
 	// 默认阈值
 	float defaultThreshold = -50;
@@ -20,7 +27,10 @@ public class ConfigActivity extends Activity {
 		setContentView(R.layout.activity_config);
 		
 		etThreshold = (EditText) super.findViewById(R.id.et_threshold);
-		etThreshold.setKeyListener(null);
+		etThreshold.setOnFocusChangeListener(new EditViewOnFocusChangeListener());
+		
+		btnSave = (Button) super.findViewById(R.id.btn_save);
+		btnSave.setOnClickListener(new SaveOnClickListener());
 		
 		Init();
 	}
@@ -46,5 +56,43 @@ public class ConfigActivity extends Activity {
 			float threshold = sharedPref.getFloat(key, defaultThreshold);
 			etThreshold.setText( String.valueOf(threshold) );
 		}
+	}
+	
+	public class EditViewOnFocusChangeListener implements OnFocusChangeListener {
+
+		@Override
+		public void onFocusChange(View v, boolean hasFocus) {
+			int id = v.getId();
+			switch (id) {
+				case R.id.et_threshold:
+					break;
+				default:
+					break;
+			}
+		}
+		
+	}
+	
+	public class SaveOnClickListener implements OnClickListener {
+
+		@Override
+		public void onClick(View v) {
+			int id = v.getId();
+			switch (id) {
+				case R.id.btn_save:
+					float threshold = Float.parseFloat(etThreshold.getText().toString());
+					Log.d("threshold", ""+threshold);
+					SharedPreferences sharedPref = getSharedPreferences(getString(R.string.s_settingsPreferences), Context.MODE_PRIVATE);
+					String key = getString(R.string.s_settingsThreshold);
+					SharedPreferences.Editor editor = sharedPref.edit();
+					editor.putFloat(key, threshold);
+					editor.commit();
+//					Toast.makeText(getApplicationContext(), "保存成功", Toast.LENGTH_SHORT);
+					break;
+				default:
+					break;
+			}
+		}
+		
 	}
 }
